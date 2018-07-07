3cf2e5c9b31fd583fd37374387f9648111f20489
David Kang <dkang009@gmail.com>
2018-07-07 18:27:21 -0500

    import index css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import NavBar from "./components/nav"
+import "./stylesheets/index.css"
 
+class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <br/>
+                <div className="container">
+                   <h1> hi pkkkk </h1>
+                </div>
+            </div>
+        );
+    }
+}
 
-const Index = () => {
-  return <div>Hello PKKKKKK</div>;
-};
 
 ReactDOM.render(<Index />, document.getElementById("index"));
 
