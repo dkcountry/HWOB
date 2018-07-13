e6f924930c93b95bb378d92d5ef5ac66f6c096ed
David Kang <dkang009@gmail.com>
2018-07-12 22:45:39 -0500

    login page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
-import NavBar from "./components/nav";
 import RouteHandler from "./main";
 import "./stylesheets/index.css";
 import { Link } from 'react-router-dom'
@@ -11,10 +10,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <NavBar />
-                <div className="container">
-                   <RouteHandler />
-                </div>
+                <RouteHandler />
             </div>
         );
     }
--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import DashBoard from "./components/dashBoard";
 import Documents from "./components/documents";
+import LoginPage from "./components/login";
 import { BrowserRouter as Router} from 'react-router-dom';
 
 
 const RouteHandler = () => (
   <main >
     <Switch>
-      <Route exact path='/' component={DashBoard}/>
+      <Route exact path='/' component={LoginPage}/>
+      <Route exact path='/dashboard' component={DashBoard}/>
       <Route path="/documents" component={Documents}/>
     </Switch>
   </main >
